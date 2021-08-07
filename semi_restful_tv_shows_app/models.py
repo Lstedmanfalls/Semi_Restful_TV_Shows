f5757791b406262fc2269ced53ca96ab7b41f0a6
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2021-08-06 18:10:13 -0600

    Added validations to forms

--- semi_restful_tv_shows_app/models.py
+++ semi_restful_tv_shows_app/models.py
@@ -1,12 +1,43 @@
 from django.db import models
-from django.db.models.fields import DateField
+from datetime import date
+
+class ShowsManager(models.Manager):
+    def create_validator(self, postData):
+        today = date.today()
+        existing_shows = Shows.objects.filter(title = postData['title'])
+        errors = {}
+        if len(postData['title']) < 2:
+            errors["title"] = "Title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Network should be at least 3 characters"
+        elif str(today) < postData['release_date']:
+            errors["release_date"] = "Release date cannot be now or in the future"
+        if len(postData["description"]) !=0 and len(postData['description']) < 10:
+            errors["description"] = "Description can be blank or at least 10 characters"
+        elif len(existing_shows) > 0:
+            errors["duplicate"] = "That show already exists"
+        return errors
+
+    def update_validator(self, postData):
+        today = date.today()
+        errors = {}
+        if len(postData['title']) < 2:
+            errors["title"] = "Title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Network should be at least 3 characters"
+        elif str(today) < postData['release_date']:
+            errors["release_date"] = "Release date cannot be now or in the future"
+        if len(postData["description"]) !=0 and len(postData['description']) < 10:
+            errors["description"] = "Description can be blank or at least 10 characters"
+        return errors
 
 class Shows(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
-    release_date = DateField()
+    release_date = models.DateField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowsManager()
     def __repr__(self):
         return f"Shows: {self.id}, {self.title}, {self.network}, {self.release_date}, {self.description}"
--- semi_restful_tv_shows_app/views.py
+++ semi_restful_tv_shows_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
 from .models import Shows
 
 def index(request): #GET REQUEST
@@ -17,11 +18,17 @@ def add_show(request): #GET REQUEST
     return render(request, "add_show.html", context)
 
 def create_show(request): #POST Request
-    if request.method != "POST":
+    errors = Shows.objects.create_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f"/shows/new_show")
+    elif request.method != "POST":
         return redirect("/")
-    if request.method == "POST":
+    elif request.method == "POST":
         create = Shows.objects.create(title = request.POST["title"], network = request.POST["network"], release_date = request.POST["release_date"], description = request.POST["description"])
         show_id = create.id
+        messages.success(request, "Show successfully created")
     return redirect(f"/shows/{show_id}")
 
 def view_show(request, show_id): #GET REQUEST
@@ -39,15 +46,21 @@ def edit_show(request, show_id): #GET REQUEST
     return render(request, "edit_show.html", context)
 
 def update_show(request, show_id): #POST Request
-    if request.method != "POST":
+    errors = Shows.objects.update_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f"/shows/{show_id}/edit")
+    elif request.method != "POST":
         return redirect("/")
-    if request.method == "POST":
+    elif request.method == "POST":
         a_show = Shows.objects.get(id=show_id)
         a_show.title = request.POST["title"]
         a_show.network = request.POST["network"]
         a_show.release_date = request.POST["release_date"]
         a_show.description = request.POST["description"]
         a_show.save()
+        messages.success(request, "Show successfully updated")
     return redirect(f"/shows/{show_id}")
 
 def delete_show(request, show_id): #POST Request
